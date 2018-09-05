86213bf38da1040ac8b6e135fa79e2e0a6586189
SnowNooDLe <sdw123123@gmail.com>
2018-09-06 00:17:00 +1200

    updating numbers

--- testing.py
+++ testing.py
@@ -1,4 +1,64 @@
-chu = ['Sakura', 'Sakura']
+# Defining empty list
+empty_list = []
+# Normal
+naruto = ['naruto']
+sakura = ['sakura']
+sasuke = ['sasuke']
+sai = ['sai']
+yamato = ['yamato']
+kabuto = ['kabuto']
+ambu = ['ambu']
+karin = ['karin']
+sasori = ['sasori']
+wood = ['wood']
 
+# Magic
+chunade = sakura + sakura
+jiraiya = yamato + naruto
+orochimaru = kabuto * 2
+temari = ambu * 2
+jyugo = karin * 2
+jezz = sasori + sai
+sasuke_1 = sasuke * 2
+chyoji = jiraiya + orochimaru + wood
 
-print (Chu)
+# Rare
+kisame = jyugo + orochimaru
+rare_tobi = jezz * 2
+sasuke_2 = sasuke_1 + kabuto
+asuma = chyoji * 2
+kamagen = jiraiya * 2
+katsyu = chunade * 2
+manda = orochimaru + sasuke + karin
+rare_kakuz = jezz + naruto + yamato
+naruto_nasun_suri = naruto * 2 + sakura
+suigets = karin + jyugo + sasuke
+kiba = kabuto + temari + sai
+rare_sasori = sasori * 2 + karin
+rare_kaara = temari * 2 + ambu
+haku = jyugo + orochimaru + ambu
+rare_kankuro = chyoji + temari + sai
+rare_roklee = chyoji + naruto + sasuke_1
+itachi = yamato * 2 + temari
+kotets = temari + yamato + chyoji
+rare_kakasi = sai + naruto + sakura + sasuke
+sino = kabuto + yamato + sasori * 2
+rare_hinata = ambu * 2 + sai + sasuke
+ino = sakura * 2 + karin + kabuto
+
+# Unique
+kurenai = kiba + rare_hinata
+unique_kakuz = rare_kakuz + kotets
+hidan = rare_kakuz + asuma
+unique_sai = sai + rare_kakasi + rare_kankuro
+unique_roklee = rare_roklee + rare_kaara + yamato
+unique_kusina = naruto_nasun_suri + rare_kankuro + jiraiya
+chojur = suigets + kisame + jezz
+unique_kakasi = rare_kakasi * 2 + jiraiya
+unique_sasuke = suigets + sasuke_2 + sasuke
+jabuja = haku + rare_kakasi + sakura
+samuii = sino + jyugo + sasuke_1
+unique_sasori = rare_sasori * 2 + sasori
+unique_hinata = rare_hinata + naruto + rare_roklee
+unique_kimimaro = manda + naruto + sasuke_1
+unique_lin = rare_kakasi + rare_tobi + sakura
